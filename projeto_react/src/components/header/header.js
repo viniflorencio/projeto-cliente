6b5562d063006674027721735a4711c7a775d0c3
viniflorencio <viniflorenciomid@gmail.com>
2022-07-20 01:25:30 -0300

    feat first commit

---
+++ projeto_react/src/components/header/header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function header() {
+  return (
+    <div>
+        <ul>
+            <img src="../components/images/user.png"/>
+            <h2> Controle Financeiro</h2>
+            <input type="checkbox" id="checkbox-menu">
+                
+            </input>
+        </ul>
+    </div>
+  )
+}
+
+export default header
---
+++ projeto_react/src/pages/mainPage.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function mainPage() {
+  return (
+    <div>mainPage</div>
+  )
+}
+
+export default mainPage
